44f05da0e15f51c41489df66849d6f859eaf192c
harfordt <tim.harford@wgc.school.nz>
2016-05-25 18:36:24 +1200

    update json for openshift

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ var standardEngine = require('./standard_info');
 
 var bodyParser = require('body-parser');
 
-
+////////////////////////
+// for openshift
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.listen(server_port, server_ip_address, function () {
-  console.log("Listening on " + server_ip_address + ", server_port " + port)
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
+/////////////////////////
 
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
@@ -46,4 +48,4 @@ app.get('/as/:number', function (req, res) {
   standardEngine.aStandard(req.params.number, res);
 });
 
-app.listen(3000);
+app.listen(8080);
